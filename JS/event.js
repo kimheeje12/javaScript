f4ede1ddbf6850a672f693290d59cbcfcc4a0ade
kimheeje12@naver.com <kimheeje12@naver.com>
2021-06-17 10:04:27 +0900

    js 추가파일

--- JS/event.js
+++ JS/event.js
@@ -1,11 +1,19 @@
 //상세 설명 보기 클릭 - 텍스트 보이기
 function showText(){
-    var text = document.getElementById("desc")
-    text.style.display = "block";
+    //var text = document.getElementById("desc");
+    //text.style.display = "block";
+      document.getElementById("desc").style.display = "block";  
+
+    // var button = document.getElementById("btn");
+    // button.style.display = "none";
+      document.getElementById("btn").style.display = "none";
 }
 
 //상세 설명 닫기 클릭 - 텍스트 숨기기
 function hideText(){
-    var text = document.getElementById("desc")
-    text.style.display = "none";
+    var text = document.getElementById("desc");
+    text.style.display = "none"; 
+
+    var button = document.getElementById("btn");
+    button.style.display = "block";
 }
